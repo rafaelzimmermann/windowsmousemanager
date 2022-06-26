6c04eb76f81ab555bf90772ee7815a8750ad405c
Rafael Zimmermann <tmpfile@outlook.de>
2022-06-26 09:16:45 +0200

    Select and display device

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DeviceList from './devices/DeviceList';
-import Device from './devices/Device';
 import ReactDOM from 'react-dom/client';
 
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className='App'>
       <DeviceList/>
-      <Device/>
     </div>
   );
 }
--- src/devices/DeviceList.js
+++ src/devices/DeviceList.js
@@ -1,45 +1,163 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-const {
-    Link
-} = ReactDOM
+import './DeviceList.css';
+import vendorIds from './vendorid'
 const {ipcRenderer} = window.require('electron')
 
 
 
 class DeviceList extends React.Component {
     state = {
-        devices: []
+        devices: [],
+        device: {}
     }
     constructor() {
         super()
         this.state.devices = ipcRenderer.sendSync('list-devices').filter(d => d["hid-info"])
-        console.log(this.state)
+        this.state.device = this.state.devices[0]
     }
     render() {
+        const selectDevice = (index) => {
+            this.state.device = this.state.devices[index]
+            this.forceUpdate()
+        }
+        const changeVerticalScroll = (event) => {
+            this.state.device.values.FlipFlopWheel = parseInt(event.target.value);
+        }
+        function toPaddedHexString(num, len) {
+            let str = num.toString(16);
+            return "0".repeat(len - str.length) + str;
+        }
+        const vendorName = () => {
+            let vendorId = this.state.device["hid-info"]["vendorId"]    
+            return vendorIds[toPaddedHexString(vendorId, 4)] || ""
+        }
         return (
             <div className='DeviceList'>
-            <table className="table">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Manufacturer</th>
-                    <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.devices.map((d, i) =>
-                        <tr key={d['path']}>
-                        <th scope="row">{i}</th>
-                        <td>
-                            <a href={"/device/" + d["path"]}>
-                                {(d['hid-info'] || {})['manufacturer'] || '-'}
-                            </a>
-                        </td>
+            <div className="container-fluid">
+                <div className="row">
+                <div className="col">
+                    <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Manufacturer</th>
+                        <th scope="col"></th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {this.state.devices.map((d, i) =>
+                            <tr key={d['path']} onClick={() => selectDevice(i)} className="clickable-row" index={i}>
+                                <th scope="row">{i}</th>
+                                <td>
+                                    {(d['hid-info'] || {})['manufacturer'] || '-'}                                        
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                    </table>
+                </div>
+                <div className="col">
+                    <div className='Device'>
+                        <nav className="navbar navbar-expand-lg bg-light">
+                            <span className="navbar-brand">Device configuration</span> 
+                        </nav>
+                        <form>
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label" htmlFor="product-id">
+                                        Manufacturer
+                                    </label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        id="product-id"
+                                        placeholder="Unknown"
+                                        value={this.state.device["hid-info"]["manufacturer"]}
+                                        disabled>
+                                    </input>
+                                    
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label" htmlFor="vendor-name">
+                                        Vendor Name
+                                    </label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        id="vendor-name"
+                                        placeholder="Unknown"
+                                        value={vendorName()}
+                                        disabled>
+                                    </input>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="form-check col">
+                                    <label className="form-label" htmlFor="product-id">
+                                        Product Id
+                                    </label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        id="product-id"
+                                        placeholder="Product Id"
+                                        value={this.state.device["hid-info"]["productId"]}
+                                        disabled>
+                                    </input>
+                                    
+                                </div>
+                                <div className="form-check col">
+                                <label className="form-label" htmlFor="vendor-id">
+                                    Vendor Id
+                                </label>
+                                <input 
+                                        type="text"
+                                        className="form-control"
+                                        id="vendor-id"
+                                        placeholder="Vendor Id"
+                                        value={this.state.device["hid-info"]["vendorId"]}
+                                        disabled>
+                                    </input>
+                                    
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="form-check col">
+                                    <input 
+                                        className="form-check-input" 
+                                        type="radio" 
+                                        name="horizontalScroolDirection" 
+                                        value="1"
+                                        checked={this.state.device["values"]["FlipFlopWheel"]["value"] == 1}
+                                        onChange={changeVerticalScroll}
+                                        id="natural-scrool">
+                                    </input>
+                                    <label className="form-check-label" htmlFor="natural-scrool">
+                                        Natural Scrool
+                                    </label>
+                                </div>
+                                <div className="form-check col">
+                                    <input 
+                                        className="form-check-input"
+                                        type="radio"
+                                        name="horizontalScroolDirection"
+                                        checked={this.state.device["values"]["FlipFlopWheel"]["value"] == 0}
+                                        onChange={changeVerticalScroll}
+                                        value="0"
+                                        id="unatual-scrool">
+                                    </input>
+                                    <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                        Default
+                                    </label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                </div>
+            </div>
             </div>
         );
     }
